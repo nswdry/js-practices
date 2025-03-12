30cdce5da14163d9f35ea8d7738d62d3bd58b1bd
nswdry <marin.taniguchi25@gmail.com>
2025-03-12 23:19:51 +0900

    [add]cal.jsにカレンダー機能を実装
    指定した年月のカレンダーを一覧表示できる機能を実装しました

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,3 +1,27 @@
-import dayjs from "dayjs";
+#!/usr/bin/env node
 
-console.log(dayjs());
+import dayjs from "dayjs";
+import minimist from "minimist";
+
+const argv = minimist(process.argv.slice(2));
+const now = dayjs();
+const year = argv.y ? Number(argv.y) : now.year();
+const month = argv.m ? Number(argv.m) : now.month() + 1;
+
+const firstDay = dayjs(`${year}-${month}-01`);
+const lastDay = firstDay.endOf("month").date();
+const daysOfWeek = firstDay.day();
+
+console.log(`      ${month}月 ${year}`);
+console.log("日 月 火 水 木 金 土");
+process.stdout.write("   ".repeat(daysOfWeek));
+
+for (let i = 1; i <= lastDay; i++) {
+  process.stdout.write(String(i).padStart(2, " ") + " ");
+  if (dayjs(`${year}-${month}-${i}`).day() === 6) {
+    console.log();
+  }
+}
+
+// zshの[%]表示をなくすため、強制的に改行
+console.log();
