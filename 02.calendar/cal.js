cd5082ea890cc5229db2ad594af0f0667798278b
nswdry <marin.taniguchi25@gmail.com>
2025-03-16 21:59:52 +0900

    [fix]lastDayの変数名と格納するデータ型を修正
    変数名をlastDate、格納するデータ型をオブジェクトへ変更しました。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -12,13 +12,13 @@ const year = argv.y >= 1970 && argv.y <= 2100 ? argv.y : now.year();
 const month = argv.m >= 1 && argv.m <= 12 ? argv.m : now.month() + 1;
 
 const firstDate = dayjs(`${year}-${month}-01`);
-const lastDay = firstDate.endOf("month").date();
+const lastDate = firstDate.endOf("month");
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 process.stdout.write("   ".repeat(firstDate.day()));
 
-for (let i = 1; i <= lastDay; i++) {
+for (let i = 1; i <= lastDate.date(); i++) {
   process.stdout.write(String(i).padStart(2, " ") + " ");
   if (dayjs(`${year}-${month}-${i}`).day() === 6) {
     console.log();
